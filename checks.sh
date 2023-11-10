02cc780e88311e2795543d63857139032621d274
dendrovis <42971143+dendrovis@users.noreply.github.com>
2023-11-10 13:15:19 +0800

    update

--- checks.sh
+++ checks.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # provide argument an identifier name
-GITHUB_REF=$1 
+BRANCH_NAME=$1 
 
 # Only valid feature branch are allow to run CI
-if [[ "${GITHUB_REF}" =~ ^refs\/heads\/feature\/[^-\/]+$ ]]; then
+if [[ "${BRANCH_NAME}" =~ ^feature\/[^-\/]+$ ]]; then
 echo "Branch name matches the pattern 'feature/' without additional subdirectories."
 else
 echo "Branch name does not match the pattern 'feature/' or has additional subdirectories."
